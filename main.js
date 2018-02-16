cb23b4752b257d9a30391113f78c7e8d5f1a7dbd
Jimmy <jimmybackstrom@hotmail.com>
2018-02-16 14:08:03 +0100

    get info frpm input to todolist

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 
-
-
 const input = document
     .getElementById('input');
 const button_NewTodo = document
     .getElementById('button_NewTodo');
 const ulTodos = document.getElementById('ulTodos');
 
-
+// Add New Todo From Input
 button_NewTodo.addEventListener('click', function(){
     const newTodoFromInput = input.value;
     const newListItem = document.createElement('li');
     newListItem.textContent = newTodoFromInput;
     ulTodos.appendChild(newListItem);
-  
 });
 
 
