2c2291e59e98a0b42a1dba5a51d160a66d18e46d
Jimmy <jimmybackstrom@hotmail.com>
2018-02-19 11:51:46 +0100

    added a button that deletes all todods that are in the checked todos column
    the button will always be on the bottom of all checked todos

--- main.js
+++ main.js
@@ -11,46 +11,47 @@ const checkedTodos = document
 
 // Add New Todo From Input
 button_NewTodo.addEventListener('click', function(){
-    const newTodoFromInput = input.value;
-    const newListItem = document.createElement('li');
-    newListItem.textContent = newTodoFromInput;
-    ulTodos.appendChild(newListItem);
+const newTodoFromInput = input.value;
+const newListItem = document.createElement('li');
+newListItem.textContent = newTodoFromInput;
+ulTodos.appendChild(newListItem);
     
-    if(document.getElementById('deleteButtonAllTodos') === null){
-        const deleteButtonAllTodos = document.createElement('button');
-    deleteButtonAllTodos.textContent ='Delete All ToDos';
-    deleteButtonAllTodos.setAttribute('id', 'deleteButtonAllTodos');
-    checkedTodos.appendChild(deleteButtonAllTodos);
-    }
     
-
+    
     //Add Butten To TodoList
     const checkedButton = document.createElement('button');
     checkedButton.textContent = 'Checked';
     newListItem.appendChild(checkedButton);
 
     
-        //Move Item From "My Todos" to "My Checked ToDos"
-        checkedButton.addEventListener('click', function(){  
-            checkedTodos.appendChild(newListItem); 
-            newListItem.removeChild(checkedButton);
+    //Move Item From "My Todos" to "My Checked ToDos"
+    checkedButton.addEventListener('click', function(){  
+    checkedTodos.appendChild(newListItem); 
+    newListItem.removeChild(checkedButton);
 
         const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete Item';
-            newListItem.appendChild(deleteButton);
+        deleteButton.textContent = 'Delete Item';
+        newListItem.appendChild(deleteButton);
 
-            deleteButton.addEventListener('click', function(){
-                checkedTodos.removeChild(newListItem);
-            })
-                
+
+    //*Add Button That Delets All ToDos in CheckedTodos Column.
+    //*if statment checks if there alLready a button because I onley want one button
+    if(document.getElementById('deleteButtonAllTodos') === null){
+        const deleteButtonAllTodos = document.createElement('button');
+        deleteButtonAllTodos.textContent ='Delete All ToDos';
+        deleteButtonAllTodos.setAttribute('id', 'deleteButtonAllTodos');
+        checkedTodos.parentElement.appendChild(deleteButtonAllTodos);
+
+        deleteButtonAllTodos.addEventListener('click', function(){
+            checkedTodos.innerHTML = '';
         });
+    };
 
-       
+        deleteButton.addEventListener('click', function(){
+        checkedTodos.removeChild(newListItem);
+        })
+                
+    });
 
-    //Remove Item From Checked Todos
-        
-    //checkedButton.addEventListener('click', function(){
-        //checkedTodos.removeChild(newListItem);
-    //});
 
 });
