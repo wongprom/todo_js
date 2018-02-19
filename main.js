09a8424cc796f1b1a5727e149c6e566d531a44ea
Jimmy <jimmybackstrom@hotmail.com>
2018-02-19 10:53:55 +0100

    made the deletebutton for every each item

--- main.js
+++ main.js
@@ -15,25 +15,42 @@ button_NewTodo.addEventListener('click', function(){
     const newListItem = document.createElement('li');
     newListItem.textContent = newTodoFromInput;
     ulTodos.appendChild(newListItem);
+    
+    if(document.getElementById('deleteButtonAllTodos') === null){
+        const deleteButtonAllTodos = document.createElement('button');
+    deleteButtonAllTodos.textContent ='Delete All ToDos';
+    deleteButtonAllTodos.setAttribute('id', 'deleteButtonAllTodos');
+    checkedTodos.appendChild(deleteButtonAllTodos);
+    }
+    
 
     //Add Butten To TodoList
     const checkedButton = document.createElement('button');
     checkedButton.textContent = 'Checked';
     newListItem.appendChild(checkedButton);
 
-    //Move Item From "My Todos" to "My Checked ToDos"
-    checkedButton.addEventListener('click', function(){  
-        ulTodos.removeChild(newListItem);
-        checkedButton.textContent = 'Delete Item';
-        checkedTodos.appendChild(newListItem);  
-            
-    });
+    
+        //Move Item From "My Todos" to "My Checked ToDos"
+        checkedButton.addEventListener('click', function(){  
+            checkedTodos.appendChild(newListItem); 
+            newListItem.removeChild(checkedButton);
+
+        const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete Item';
+            newListItem.appendChild(deleteButton);
+
+            deleteButton.addEventListener('click', function(){
+                checkedTodos.removeChild(newListItem);
+            })
+                
+        });
+
+       
 
     //Remove Item From Checked Todos
         
-    checkedButton.addEventListener('click', function(){
-        checkedTodos.removeChild(newListItem);
-    });
+    //checkedButton.addEventListener('click', function(){
+        //checkedTodos.removeChild(newListItem);
+    //});
 
 });
-
